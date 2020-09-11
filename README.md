6d04e50addffa0c7c8f5d44cc4076173123586d0
armiin-k <arminggwp@gmail.com>
2020-09-12 01:57:32 +0200

    third commit

--- README.md
+++ README.md
@@ -5,3 +5,8 @@ Made with HTML, CSS and Javascript.
 -second commit: 
 Fixed some bugs and changed the UI a little, still some bugs left to iron out. 
 Thinking of rewriting and simplifying the backend, possibly using a different method but we'll see in the third commit/update. 
+
+-third commit: 
+Rewrote entire backend to simplify code and fix all bugs I could find. 
+Tweaked UI colors and added 'Help' button for explaining keyboard keys.
+Also finished with keyboard support. Now it meets all of the criteria.
--- calcBackend.js
+++ calcBackend.js
@@ -1,88 +1,165 @@
-/* calculator backend */
+// calculator backend v2 
 
-/* global variables */
-let initialNumbers = [];
-let finalNumbers = [];
+//global variables
 var firstDisplay = document.getElementById('display');
 var secondDisplay = document.getElementById('operationDisplay');
-var counter = 0;
-var newCounter = 0;
-var result = 0;
+let initialNumArray = []
+let numArray = [];
+let result = 0;
+let operator = '';
+let numbers = [];
+let counter = 0;
+let resultCounter = 0;
 
-/* makes number buttons work */
-function displayText (numValue) {
-    if (numValue === parseInt(numValue, 10)) {
-        firstDisplay.innerHTML += (numValue);
-        initialNumbers.push(numValue);
-        console.log(initialNumbers);
-        //makes operator buttons work
-    }else if (numValue !== parseInt(numValue, 10) && counter == 0) { 
-        finalNumbers.push(initialNumbers.join(''));
-        firstDisplay.innerHTML = finalNumbers + ' ' + numValue + ' ';
-        initialNumbers = [];
-        counter += 1;
-        //checks if operator has already been pressed
-    }else if (numValue !== parseInt(numValue, 10) && counter == 1) {
-        operate();
-        firstDisplay.innerHTML = result + ' ' + numValue + ' ';
-        secondDisplay.innerHTML = '';
-        finalNumbers = [];
-        initialNumbers = [];
-        finalNumbers.push(result);
-    }
-}
-
-/* function for backspace key */
-function backspaceKey () {
-    initialNumbers.pop();
-    console.log(initialNumbers);
-    firstDisplay.innerHTML = initialNumbers.join('');
-}
-
-/* function that takes everything and spits out the result */
-function operate() {
+/* function for numpad keys. It also checks if there is a result,
+    meaning if a number is pressed a new calculation is started,
+    instead of taking the result as the first number */
+function numPad (numValue) {
     if (secondDisplay.innerHTML.indexOf('=') == -1) {
-        finalNumbers.push(initialNumbers.join(''));
-        secondDisplay.innerHTML = firstDisplay.innerHTML;
-        console.log(secondDisplay.innerHTML);
-        console.log(finalNumbers);
-        let numbers = finalNumbers.map(Number);
-        console.log(numbers);
-        //checks which operator was pressed to calculate and print result
-        if (firstDisplay.innerHTML.indexOf('+') !== -1) {
-            result = numbers[0] + numbers[1];
-            secondDisplay.innerHTML += ' = ' + result;
-        }else if (firstDisplay.innerHTML.indexOf('-') !== -1) {
-            result = numbers[0] - numbers[1];
-            secondDisplay.innerHTML += ' = ' + result;
-        }else if (firstDisplay.innerHTML.indexOf('*') !== -1) {
-            result = numbers[0] * numbers[1];
-            secondDisplay.innerHTML += ' = ' + result;
-        }else if (firstDisplay.innerHTML.indexOf('/') !== -1) {
-            result = numbers[0] / numbers[1];
-            secondDisplay.innerHTML += ' = ' + result;
-        }
-    }else {
-        return;
+        firstDisplay.innerHTML += numValue;
+        initialNumArray.push(numValue);
+    }else if (secondDisplay.innerHTML.indexOf('=') !== -1) {
+        counter = 0;
+        resultCounter = 0;
+        result = 0;
+        secondDisplay.innerHTML = '';
+        firstDisplay.innerHTML += numValue;
+        initialNumArray.push(numValue);
     }
-    firstDisplay.innerHTML = '';
 }
-/* this clears everything for new calculations */
+
+/* function for the operator buttons which also checks if an operator
+    has already been pressed */
+function operatorButtons (operation) {
+    if (counter == 0) {
+        operator = operation;
+        firstDisplay.innerHTML = '';
+        numArray.push(initialNumArray.join(''));
+        initialNumArray = [];
+        secondDisplay.innerHTML += numArray + ' ' + operator;
+        return counter = 1;
+    }else if (counter == 1) {
+        if (resultCounter == 1) {
+            numArray.push(result);
+            operator = operation;
+            initialNumArray = [];
+            secondDisplay.innerHTML = numArray + ' ' + operator;
+            counter = 1;
+            resultCounter = 0;
+        }else if (resultCounter == 0) {
+            operator = operation;
+            equals();
+            resultCounter = 1;
+        }
+    }
+}
+
+/* function for equals key, looks like it does a lot but just
+    calculates, displays result and resets for new calculations */
+function equals () {
+    if (operator == '+') {
+        numArray.push(initialNumArray.join(''));
+        numbers = numArray.map(Number);
+        result = numbers[0] + numbers[1];
+        secondDisplay.innerHTML += ' ' + numArray[1] + ' = ' + result;
+        firstDisplay.innerHTML = '';
+        initialNumArray = [];
+        numArray = [];
+        resultCounter = 1;
+        counter = 1;
+        return result;
+    }else if (operator == '-') {
+        numArray.push(initialNumArray.join(''));
+        numbers = numArray.map(Number);
+        result = numbers[0] - numbers[1];
+        secondDisplay.innerHTML += ' ' + numArray[1] + ' = ' + result;
+        firstDisplay.innerHTML = '';
+        initialNumArray = [];
+        numArray = [];
+        resultCounter = 1;
+        counter = 1;
+        return result;
+    }else if (operator == '*') {
+        numArray.push(initialNumArray.join(''));
+        numbers = numArray.map(Number);
+        result = numbers[0] * numbers[1];
+        secondDisplay.innerHTML += ' ' + numArray[1] + ' = ' + result;
+        firstDisplay.innerHTML = '';
+        initialNumArray = [];
+        numArray = [];
+        resultCounter = 1;
+        counter = 1;
+        return result;
+    }else if (operator == '/') {
+        numArray.push(initialNumArray.join(''));
+        numbers = numArray.map(Number);
+        result = numbers[0] / numbers[1];
+        secondDisplay.innerHTML += ' ' + numArray[1] + ' = ' + result;
+        firstDisplay.innerHTML = '';
+        initialNumArray = [];
+        numArray = [];
+        resultCounter = 1;
+        counter = 1;
+        return result;
+    }
+}
+
+function deleteKey () {
+    initialNumArray.pop()
+    firstDisplay.innerHTML = initialNumArray.join('');
+}
+
 function clearAll() {
-    initialNumbers = [];
-    finalNumbers = [];
+    initialNumArray = []
+    numArray = [];
+    result = 0;
+    operator = '';
+    numbers = [];
+    counter = 0;
+    resultCounter = 0;
     firstDisplay.innerHTML = '';
     secondDisplay.innerHTML = '';
-    counter = 0;
-    result = 0;
 }
 
-/* 
-TODO:
-- equals button pressed twice bug - DONE (i hope)
-- result shouldn't always be first number bug
-- dot operator functionality
-- keyboard support
-- test everything for final github commit
-OBJECTS CAN HAVE NAMES I CAN TRACK RESULT AND DISCARD IT IF I NEED TO OOOOMGGGGGG
-*/
+// function to bind keyboard keys
+document.addEventListener('keyup', function(e) {
+    if(event.key === '1') {
+        numPad(1);
+    }else if (event.key === '2') {
+        numPad(2);
+    }else if (event.key === '3') {
+        numPad(3);
+    }else if (event.key === '4') {
+        numPad(4);
+    }else if (event.key === '5') {
+        numPad(5);
+    }else if (event.key === '6') {
+        numPad(6);
+    }else if (event.key === '7') {
+        numPad(7);
+    }else if (event.key === '8') {
+        numPad(8);
+    }else if (event.key === '9') {
+        numPad(9);
+    }else if (event.key === '0') {
+        numPad(0);
+    }else if (event.key === '+') {
+        operatorButtons('+');
+    }else if (event.key === '-') {
+        operatorButtons('-');
+    }else if (event.key === '*') {
+        operatorButtons('*');
+    }else if (event.key === '/') {
+        operatorButtons('/');
+    }else if (event.key === 'Enter') {
+        equals();
+    }else if (event.key === 'Backspace') {
+        deleteKey();
+    }else if (event.key === 'c') {
+        clearAll();
+    }
+})
+
+function showHelp() {
+    document.getElementById('showHelp').style.display = "flex";
+}
